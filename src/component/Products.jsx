e3b1ae97c2f9870e7c1670eebbdb5d741de0fd04
Anony <Anonymakon@gmail.com>
2024-01-29 23:24:48 -0800

    update

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { product } from '../../data';
+// import { product } from '../../data';
 
 const Products = () => {
   return (
     <div className="flex flex-wrap bg-white">
-      {product.map((item, index) => (
+      {/* {product.map((item, index) => (
         <div
           key={index}
           className={`p-4 py-10 text-center flex flex-col items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 text-${item.color}`}
@@ -13,7 +13,7 @@ const Products = () => {
           <img src={item.img} alt="Dr Pepper" className="max-w-32 h-auto mb-6" />
           <h3 className="text-xl font-extrabold font-sans">{item.title}</h3>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
